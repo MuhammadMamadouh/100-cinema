e5c6be4bb9991936be96813b610822a67fd441d0
Muhammad Mamdouh <muhammadmamdouh93@gmail.com>
2019-01-12 22:47:08 +0200

    -- update database config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_2ac9b8cab757c19'),
+            'username' => env('DB_USERNAME', 'b427ec448249ae'),
+            'password' => env('DB_PASSWORD', '3debfd98'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
